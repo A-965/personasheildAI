15a19f7220dc3b03cca53ef7b61563b8b95ee16c
Ajay Kadian <ajaykadian@Ajays-MacBook-Air.local>
2026-04-18 06:30:38 +0530

    Update API baseURL for production deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: window.location.hostname === 'localhost' 
+    ? 'http://localhost:8000/api' 
+    : 'https://personasheildai.onrender.com/api',
 });
 
 export const analyzeApi = {
