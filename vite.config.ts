51f882b3d2d859fe7518a54332e25f84647978da
Ajay Kadian <ajaykadian@Ajays-MacBook-Air.local>
2026-04-18 06:22:19 +0530

    Optimize Vite build chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,16 @@ export default defineConfig({
     outDir: 'dist',
     minify: 'terser',
     sourcemap: true,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
     commonjsOptions: {
       include: [/node_modules/]
     }
